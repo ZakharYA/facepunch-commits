d1a7a51b96e00c8ed9bb599ec74d422a7bbbe77a
Zakhar <zaharsd7@gmail.com>
2020-09-12 16:53:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+### NPM
+```
+npm i facepunch-api
+```
+
+## Example
+```js
+const _facepunchAPI = require('facepunch-api');
+const facepunchAPI = new _facepunchAPI(5000);
+
+facepunchAPI.subscribe('SpaceUsurperUnity', (commit) => {
+	console.log('Hello Garry! New commit from SpaceUsurperUnity:', commit);
+});
+
+facepunchAPI.subscribe('Garrys Mod', (commit) => {
+	console.log('Hello Garry! New commit from Garrys Mod:', commit);
+});
+```
+<a href="https://www.npmjs.com/package/facepunch-lib"><img src="https://img.shields.io/npm/v/facepunch-lib.svg?style=flat-square" alt="NPM version"></a>
+<a href="https://vk.com/ghost1337gg"><img src="https://brand.vkforms.ru/static/media/logo_color_154.08f6e176.svg" height=20></a>
---
+++ src/index.js
@@ -0,0 +1,71 @@
+// import fetch from 'node-fetch';
+const fetch = require('node-fetch');
+
+class _FACEPUNCHAPI {
+	/**
+	 * constructor
+	 * @param {Number} interval interval from request to api
+	 */
+	constructor(interval) {
+		this.latestCommit = {};
+		this.interval = interval;
+
+		this.options = {
+			api: 'https://commits.facepunch.com/r/'
+		}
+	}
+
+	/**
+	 * sendRequest
+	 * * send request from api facepunch and get commits
+	 * @param {String} repository name repository to get
+	 * @returns return object commits
+	 */
+	async sendRequest(repository) {
+
+		const request = await fetch(`${this.options.api}${repository}?format=json`);
+		const result = await request.json();
+
+		return result.results;
+	}
+
+	/**
+	 * subscribe
+	 * * subscribe event from repository, call callback function
+	 * @param {String} repository name repository to get
+	 * @param {Function} callback how to return commit function
+	 */
+	async subscribe(repository, callback) {
+		const request = await this.sendRequest(repository);
+		this.latestCommit[repository] = request[0].id - 1;
+
+		setInterval(async () => {
+			const request = await this.sendRequest(repository);
+			request.map((e) => {
+				if (e.id <= this.latestCommit[repository]) return;
+				callback(e);
+			})
+
+			this.latestCommit[repository] = request[0].id;
+
+		}, this.interval);
+	}
+}
+
+// example, kek :/
+// (() => {
+// 	const facepunchAPI = new _FACEPUNCHAPI(5000);
+
+// 	facepunchAPI.subscribe('SpaceUsurperUnity', (commit) => {
+// 		console.log('Hello Garry! New commit from SpaceUsurperUnity:', commit);
+// 	});
+
+// 	facepunchAPI.subscribe('Garrys Mod', (commit) => {
+// 		console.log('Hello Garry! New commit from Garrys Mod:', commit);
+// 	});
+
+// })();
+
+// export default _FACEPUNCHAPI;
+
+module.exports = _FACEPUNCHAPI;
