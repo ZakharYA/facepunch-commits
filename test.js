ae4eb1301b98d4b2790fdad5e40bc1a304311be4
Zakhar <zaharsd7@gmail.com>
2021-01-09 16:58:44 +0700

    changed my test

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ const FacepunchCommits = require('./dist/index.js');
 
 const commits = new FacepunchCommits(1000);
 
+commits.latestCommit[''] = 0;
+
 commits.subscribeToAll((commit) => {
-	console.log(commit.id);
+	console.log(commit);
 })
